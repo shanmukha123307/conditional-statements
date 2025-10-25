1a8d0e314e03a3ffd100745c6d5495cb0a7d446f
shanmukha123307 <anusha.palgot@gmail.com>
2025-10-25 21:01:26 +0530

    Delete file2.py

--- file2.py
+++
@@ -1,7 +0,0 @@
-cost=float(input("please enter the prise___"))
-sp=float(input("please enter the sale amount___"))
-if sp>cost:
-    amount=sp-cost
-    print("total profit is {0}".format(amount))
-else:
-    print("NO PROHIT!!!!")
